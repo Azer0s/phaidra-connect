636452cf10fb08cead122d9b0ce89d9687e10eaf
Azer0s <ariel@simulevski.at>
2024-06-18 17:24:18 +0200

    Add form upload

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module phaidra-connect
 
 go 1.22.0
 
-require github.com/nats-io/nats.go v1.33.1
+require (
+	github.com/google/uuid v1.6.0
+	github.com/nats-io/nats.go v1.33.1
+)
 
 require (
 	github.com/klauspost/compress v1.17.2 // indirect
--- main.go
+++ main.go
@@ -1,26 +1,142 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/google/uuid"
 	"github.com/nats-io/nats.go"
+	"io"
+	"mime/multipart"
+	"net/http"
 	"os"
+	"path"
+	"text/template"
 )
 
 var conn *nats.Conn
 
 type config struct {
-	natsHost   string
-	museumHost string
+	natsHost        string
+	museumHost      string
+	phaidraHost     string
+	phaidraUser     string
+	phaidraPassword string
+	templateFile    string
+	template        *template.Template
+}
+
+type PhaidraMetadata struct {
+	Title        string
+	Description  string
+	ResourceLink string
+}
+
+func createPhaidraObject(conf config, metadata PhaidraMetadata) error {
+	//curl -X POST -u user:pass "https://sandbox.phaidra.org/api/resource/create" -F "metadata=@resource_metadata.json"
+
+	// post request to phaidra api (placeholder metadata for now)
+	apiPath := "/api/resource/create"
+
+	// add form file (metadata as json-ld)
+	buf := new(bytes.Buffer)
+	multipartWriter := multipart.NewWriter(buf)
+	defer func(multipartWriter *multipart.Writer) {
+		err := multipartWriter.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(multipartWriter)
+
+	metadataBuf := new(bytes.Buffer)
+	err := conf.template.Execute(metadataBuf, metadata)
+	if err != nil {
+		return err
+	}
+
+	tmpMetadataFile := path.Join(os.TempDir(), uuid.New().String()+".json")
+	defer func() {
+		err := os.Remove(tmpMetadataFile)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	file, err := os.Create(tmpMetadataFile)
+	if err != nil {
+		return err
+	}
+
+	_, err = file.WriteString(metadataBuf.String())
+	if err != nil {
+		return err
+	}
+
+	part, err := multipartWriter.CreateFormFile("metadata", file.Name())
+	if err != nil {
+		return err
+	}
+
+	_, err = file.Seek(0, 0)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(part, file)
+	if err != nil {
+		return err
+	}
+
+	request, err := http.NewRequest(http.MethodPost, conf.phaidraHost+apiPath, buf)
+	if err != nil {
+		return err
+	}
+
+	request.Header.Set("Content-Type", multipartWriter.FormDataContentType())
+	request.SetBasicAuth(conf.phaidraUser, conf.phaidraPassword)
+
+	res, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return err
+	}
+
+	resBody := new(bytes.Buffer)
+	_, err = resBody.ReadFrom(res.Body)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(resBody.String())
+	return nil
 }
 
 func main() {
 	conf := config{
-		natsHost:   os.Getenv("NATS_HOST"),
-		museumHost: os.Getenv("MUSEUM_HOST"),
+		natsHost:        os.Getenv("NATS_HOST"),
+		museumHost:      os.Getenv("MUSEUM_HOST"),
+		phaidraHost:     os.Getenv("PHAIDRA_HOST"),
+		phaidraUser:     os.Getenv("PHAIDRA_USER"),
+		phaidraPassword: os.Getenv("PHAIDRA_PASSWORD"),
+		templateFile:    os.Getenv("TEMPLATE_FILE"),
 	}
 
-	var err error
+	tmpl, err := template.ParseFiles(conf.templateFile)
+	if err != nil {
+		panic(err)
+	}
+
+	conf.template = tmpl
+
+	err = createPhaidraObject(conf, PhaidraMetadata{
+		Title:        "Test",
+		Description:  "Test",
+		ResourceLink: "https://sandbox.phaidra.org/objects/1",
+	})
+	if err != nil {
+		return
+	}
+	return
+
 	conn, err = nats.Connect(conf.natsHost)
 	if err != nil {
 		panic(err)
